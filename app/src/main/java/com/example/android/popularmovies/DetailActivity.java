c60f8d4f36bafd46bf17597e80eb7a98a3841ecf
jbfoster <justinfost@gmail.com>
2016-05-10 23:51:32 -0700

    Gets youtube links for movie trailers and displays them in detail view

--- app/src/main/java/com/example/android/popularmovies/DetailActivity.java
+++ app/src/main/java/com/example/android/popularmovies/DetailActivity.java
@@ -16,17 +16,36 @@
 
 package com.example.android.popularmovies;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 // DetailActivy is executed when a movie poster is clicked to display information for
 // the specific movie that was selected
@@ -60,16 +79,21 @@ public class DetailActivity extends AppCompatActivity {
     // DetailFragment displays the information for a selected movie
     public static class DetailFragment extends Fragment {
 
+        public View rootView; // global variable to store base linear layout view
+        String[] movieStr; // array to hold movie data
+        public String[] trailersInfo; // array to store trailers data
+
+
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
 
             Intent intent = getActivity().getIntent();
-            View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+            rootView = inflater.inflate(R.layout.fragment_detail, container, false);
 
             // Pull data sent with intent and assign the appropriate data values
             if (intent != null && (intent.getExtras() != null)) {
-                String[] movieStr = intent.getStringArrayExtra("data");
+                movieStr = intent.getStringArrayExtra("data");
                 ((TextView) rootView.findViewById(R.id.title_text)).setText(movieStr[0]);
                 ImageView imageView = (ImageView) rootView.findViewById(R.id.poster_image);
                 String url = "http://image.tmdb.org/t/p/w185/";
@@ -83,5 +107,183 @@ public class DetailActivity extends AppCompatActivity {
             }
             return rootView;
         }
+
+        @Override
+        public void onStart() {
+            super.onStart();
+            getTrailers();
+        }
+
+        // getTrailers method gets trailers by executing FetchTrailersTask
+        private void getTrailers() {
+            // FetchTrailersTask gets trailers from The Movie Database
+            FetchTrailersTask trailersTask = new FetchTrailersTask();
+            if (isNetworkAvailable()) { // only download reviews if network is available
+                trailersTask.execute();
+            }
+        }
+
+        // isNetworkAvailable() checks for network connectivity
+        public boolean isNetworkAvailable() {
+            ConnectivityManager cm = (ConnectivityManager)
+                    getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+            // if no network is available networkInfo will be null
+            // otherwise check if we are connected
+            if (networkInfo != null && networkInfo.isConnected()) {
+                return true;
+            }
+            return false;
+        }
+
+        // FetchTrailersTask class contains methods for getting trailers from The Movie Database
+        public class FetchTrailersTask extends AsyncTask<String, Void, String[]> {
+
+            private final String LOG_TAG = FetchTrailersTask.class.getSimpleName();
+
+            /**
+             * Take the String representing trailers in JSON Format and parse data
+             */
+            private String[] getTrailersFromJson(String trailersJsonStr)
+                    throws JSONException {
+
+                // These are the names of the JSON objects that need to be extracted.
+                final String MDB_RESULTS = "results";
+                final String MDB_LINK = "key";
+
+                JSONObject trailersJson = new JSONObject(trailersJsonStr);
+                JSONArray trailersArray = trailersJson.getJSONArray(MDB_RESULTS);
+
+                // Create array to hold reviews
+                trailersInfo = new String[trailersArray.length()];
+
+                String[] resultStrs = new String[trailersArray.length()];
+                for (int i = 0; i < trailersArray.length(); i++) {
+
+                    // Get the JSON object representing the ith trailer
+                    JSONObject movieData = trailersArray.getJSONObject(i);
+
+                    // Store the data for each trailer in trailersInfo array
+                    trailersInfo[i] = movieData.getString(MDB_LINK);
+
+                    resultStrs[i] = trailersInfo[i];
+                }
+
+                return resultStrs;
+            }
+
+
+            // doInBackground executes the API call in a background thread
+            @Override
+            protected String[] doInBackground(String... params) {
+                String movieId = movieStr[5];
+
+//                Intent intent = getActivity().getIntent();
+//                if (intent != null && (intent.getExtras() != null)) {
+//                    String[] movieStr = intent.getStringArrayExtra("data");
+//                    movieId = movieStr[5];
+//                }
+
+                // These two need to be declared outside the try/catch
+                // so that they can be closed in the finally block.
+                HttpURLConnection urlConnection = null;
+                BufferedReader reader = null;
+
+                // Will contain the raw JSON response as a string.
+                String trailersJsonStr = null;
+
+                try {
+
+                    // Construct the URL for the The Movie Database query
+                    // Information and possible parameters are available at themoviedb.org
+                    final String FORECAST_BASE_URL = "http://api.themoviedb.org/3/movie/";
+                    final String API_KEY = "api_key";
+                    // api key for The Movie Database is stored in api-keys.xml resource file
+                    final String KEY = getString(R.string.movie_api_key);
+
+                    String urlString = FORECAST_BASE_URL + movieId + "/" + "videos";
+
+                    Uri builtUri = Uri.parse(urlString).buildUpon()
+                            .appendQueryParameter(API_KEY, KEY)
+                            .build();
+
+                    URL url = new URL(builtUri.toString());
+
+                    // Create the request to The Movie Database, and open the connection
+                    urlConnection = (HttpURLConnection) url.openConnection();
+                    urlConnection.setRequestMethod("GET");
+                    urlConnection.connect();
+
+                    // Read the input stream into a String
+                    InputStream inputStream = urlConnection.getInputStream();
+                    StringBuffer buffer = new StringBuffer();
+                    if (inputStream == null) {
+                        // Nothing to do.
+                        return null;
+                    }
+                    reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                        // But it does make debugging a *lot* easier if you print out the completed
+                        // buffer for debugging.
+                        buffer.append(line + "\n");
+                    }
+
+                    if (buffer.length() == 0) {
+                        // Stream was empty.  No point in parsing.
+                        return null;
+                    }
+                    trailersJsonStr = buffer.toString();
+
+                } catch (IOException e) {
+                    Log.e(LOG_TAG, "Error ", e);
+                    // If the code didn't successfully get the movie data, there's no point in
+                    // attempting to parse it.
+                    return null;
+                } finally {
+                    if (urlConnection != null) {
+                        urlConnection.disconnect();
+                    }
+                    if (reader != null) {
+                        try {
+                            reader.close();
+                        } catch (final IOException e) {
+                            Log.e(LOG_TAG, "Error closing stream", e);
+                        }
+                    }
+                }
+
+                try {
+                    return getTrailersFromJson(trailersJsonStr);
+                } catch (JSONException e) {
+                    Log.e(LOG_TAG, e.getMessage(), e);
+                    e.printStackTrace();
+                }
+
+                return null;
+            }
+
+            // Once background thread is completed, add text views for each review
+            @Override
+            protected void onPostExecute(String[] result) {
+                if (result != null) {
+                    LinearLayout linearLayout = (LinearLayout)
+                            rootView.findViewById(R.id.detail_linear_layout);
+
+                    // cycle through each review and add text views for author and review
+                    for (String trailer : trailersInfo) {
+                        String author = "Trailer " + trailer + ":";
+                        TextView authorText = new TextView(getActivity());
+                        authorText.setText(author);
+                        authorText.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 20);
+                        authorText.setPadding(0, 0, 0, 10);
+                        linearLayout.addView(authorText);
+                    }
+                }
+            }
+        }
+
     }
 }
